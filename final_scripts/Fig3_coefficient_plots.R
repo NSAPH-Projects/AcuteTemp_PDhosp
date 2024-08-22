a0f698396c8b01a24a09e6a753b9348f556967e7
Lauren Mock <laurenmock17@gmail.com>
2024-08-22 14:41:35 -0400

    clean up fig 3 plot

--- final_scripts/Fig3_coefficient_plots.R
+++ final_scripts/Fig3_coefficient_plots.R
@@ -25,7 +25,7 @@ perc <- 1:999/1000
 
 #---------- data by climate type for sex = female
 
-plot_erc_F <- #rbindlist(lapply(names(cp_F), function(s) {
+dat_F <- #rbindlist(lapply(names(cp_F), function(s) {
   rbindlist(lapply(names(cp_F), function(c) {
     rbindlist(lapply(c(500, 900, 950, 990), function(p) {
       data.frame(#subgroup = s,
@@ -39,12 +39,12 @@ plot_erc_F <- #rbindlist(lapply(names(cp_F), function(s) {
     }))
   }))
 #}))
-plot_erc_F$sex <- "Female"
+dat_F$sex <- "Female"
 
 
 #---------- data by climate type for sex = male
 
-plot_erc_M <- #rbindlist(lapply(names(cp_M), function(s) {
+dat_M <- #rbindlist(lapply(names(cp_M), function(s) {
   rbindlist(lapply(names(cp_M), function(c) {
     rbindlist(lapply(c(500, 900, 950, 990), function(p) {
       data.frame(#subgroup = s,
@@ -58,35 +58,89 @@ plot_erc_M <- #rbindlist(lapply(names(cp_M), function(s) {
     }))
   }))
 #}))
-plot_erc_M$sex <- "Male"
+dat_M$sex <- "Male"
 
 
-# combine female and male
-plot_erc_FM <- rbind(plot_erc_F, plot_erc_M)
+# # combine female and male
+# plot_erc_FM <- rbind(plot_erc_F, plot_erc_M)
+
+
+#---------- data for the whole country for sex = female
+
+dat_all_F <- #rbindlist(lapply(names(cp_F), function(s) {
+  #rbindlist(lapply(names(cp_F), function(c) {
+    rbindlist(lapply(c(500, 900, 950, 990), function(p) {
+      data.frame(#subgroup = s,
+        ctype = "Nationwide",
+        #n = cp_F[[s]]$n,
+        perc = perc[p],
+        val = cp_all_F$predvar[p],
+        lag = 0:14,
+        fit = cp_all_F$cumfit[p,],
+        se = cp_all_F$cumse[p,])
+    }))
+  #}))
+#}))
+dat_all_F$sex <- "Female"
+
+
+#---------- data for the whole country for sex = male
+
+dat_all_M <- #rbindlist(lapply(names(cp_M), function(s) {
+  #rbindlist(lapply(names(cp_M), function(c) {
+  rbindlist(lapply(c(500, 900, 950, 990), function(p) {
+    data.frame(#subgroup = s,
+      ctype = "Nationwide",
+      #n = cp_M[[s]]$n,
+      perc = perc[p],
+      val = cp_all_M$predvar[p],
+      lag = 0:14,
+      fit = cp_all_M$cumfit[p,],
+      se = cp_all_M$cumse[p,])
+  }))
+#}))
+#}))
+dat_all_M$sex <- "Male"
+
+
+
+#-----------
+
+# bind all dfs
+dat_fig3 <- rbind(dat_F, dat_M, dat_all_F, dat_all_M)
+
+# new columns for OR and 95% CI
+dat_fig3 <- dat_fig3 %>%
+  mutate(or = exp(fit),
+         or_low = or - 1.96*se,
+         or_high = or + 1.96*se)
 
 # restrict to rows of interest
-plot_erc_FM <- plot_erc_FM[lag == 3 & val == 0.990]
+dat_fig3 <- dat_fig3[lag == 2 & val == 0.990]
 
 
 
 #---------- data across all climate types and both sexes
-plot_erc_all <- #rbindlist(lapply(names(cp_M), function(s) {
-  #rbindlist(lapply(names(cp_all), function(c) {
-  rbindlist(lapply(c(500, 900, 950, 990), function(p) {
-    data.frame(#subgroup = s,
-      #ctype = c,
-      #n = cp_all[[s]]$n,
-      perc = perc[p],
-      val = cp_all$predvar[p],
-      lag = 0:14,
-      fit = cp_all$cumfit[p,],
-      se = cp_all$cumse[p,])
-  }))
-#}))
-#}))
 
-# get overall OR
-combined_fit_OR <- exp(plot_erc_all[lag == 3 & val == 0.990, fit])
+# this was previously the black dashed line
+
+# plot_erc_all <- #rbindlist(lapply(names(cp_M), function(s) {
+#   #rbindlist(lapply(names(cp_all), function(c) {
+#   rbindlist(lapply(c(500, 900, 950, 990), function(p) {
+#     data.frame(#subgroup = s,
+#       #ctype = c,
+#       #n = cp_all[[s]]$n,
+#       perc = perc[p],
+#       val = cp_all$predvar[p],
+#       lag = 0:14,
+#       fit = cp_all$cumfit[p,],
+#       se = cp_all$cumse[p,])
+#   }))
+# #}))
+# #}))
+# 
+# # get overall OR
+# combined_fit_OR <- exp(plot_erc_all[lag == 3 & val == 0.990, fit])
 
 
 
@@ -94,30 +148,46 @@ combined_fit_OR <- exp(plot_erc_all[lag == 3 & val == 0.990, fit])
 ########################### NEW FIGURE 3 ###############################
 ########################################################################
 
+# make climate type factor to set order
+dat_fig3$ctype <- factor(dat_fig3$ctype,
+                       levels = c("Nationwide", "Temperate", "Continental", "Arid", "Tropical"))
+
 # plot
-pdf(paste0(dir_figs, "fig3_coefficients.pdf"), height = 4, width = 6)
-plot_erc_FM[lag == 3 & val == 0.990] |>
-  ggplot(aes(x = sex, 
-             y = exp(fit), 
-             #color = sex, 
-             group = sex,
-             ymin = exp(fit - 2*se), ymax = exp(fit + 2*se))) +
-  geom_hline(yintercept = combined_fit_OR, linetype = 3, size = 0.5) +
-  geom_hline(yintercept = 1, linetype = 1, size = 0.5, color = "red") +
+pdf(paste0(dir_figs, "fig3_coefficients.pdf"), height = 2.5, width = 6)
+dat_fig3 |>
+  ggplot(aes(x = sex, y = or, 
+             color = sex, group = sex,
+             ymin = or_low, ymax = or_high)) +
+  #geom_hline(yintercept = combined_fit_OR, linetype = 3, size = 0.5) +
+  #geom_hline(yintercept = 1, linetype = 1, linewidth = 0.5, color = "red") +
+  geom_hline(yintercept = 1, linetype = 2, linewidth = 0.5, color = "gray50") +
   geom_errorbar(size = 0.7, width = 0) +
   geom_point(size = 2) +
-  theme_minimal(
-    #base_size = 24
-    ) +
-  theme(legend.position = "none",
-    axis.text.x = element_text(angle = 45, hjust = 1),
-    axis.line = element_line(),
-    axis.ticks = element_line()) +
+  scale_fill_manual(values = c("Female" = "gold3",
+                               "Male" = "blue3")) +
+  scale_color_manual(values = c("Female" = "gold3",
+                                "Male" = "blue3")) +
+  #theme_minimal(base_size = 12) +
+  theme_light(base_size = 10) +
+  theme(
+    #legend.position = "none",
+    #axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank(),
+    #axis.line = element_line(),
+    #axis.ticks = element_line(),
+    #panel.spacing = unit(1, "lines"),
+    strip.background = element_blank(),
+    strip.text = element_text(color = "black")
+  ) +
   facet_grid(~ ctype#, 
              #scales = "free_x", space = "free_x"
   ) +
   # scale_y_continuous(limits = c(0.99, 1.18)) +
-  labs(x = "", y = "Odds ratio of hospitalization", color = "", fill = "")
+  labs(x = "", 
+       y = "Odds ratio", 
+       color = "", 
+       fill = "")
 dev.off()
 
 
