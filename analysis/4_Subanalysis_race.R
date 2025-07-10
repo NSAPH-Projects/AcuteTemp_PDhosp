15f7e83afb875727735813ead594863dae214096
Lauren Mock <laurenmock17@gmail.com>
2025-07-10 17:09:56 -0400

    race-stratified analysis

---
+++ analysis/4_Subanalysis_race.R
@@ -0,0 +1,270 @@
+library(data.table)
+library(fst)
+library(ggplot2)
+library(survival)
+library(dlnm)
+library(ggrepel)
+library(cowplot)
+
+dir_data <- "/n/dominici_nsaph_l3/Lab/projects/cdinehart_PDhosp_temperature/data/"
+data <- read_fst(paste0(dir_data, 'data_complete_climate.fst'), as.data.table = TRUE)
+
+#Remove rows with HI values below 0 
+data <- data[data$HI_lag_0 >= 0, ]
+
+#----- clean up race variable for stratification
+# remove missing race
+data <- data[race != 0,]
+
+# make a new column with race name
+race_labels <- c("1" = "white", 
+                 "2" = "black", 
+                 "3" = "other",
+                 "4" = "asian",
+                 "5" = "hispanic",
+                 "6" = "native")
+data[, race_name := race_labels[as.character(race)]]
+
+
+# Climate type models -----------------------
+
+# loop through each racial group
+for(i in 1:length(race_labels)){
+  
+  # restrict to a single racial group
+  cat("Race: ", race_labels[i])
+  data_onerace <- data[race_name == race_labels[i]]
+  
+  #initial models, no lag
+  ##By climate region 
+  model_temp <- clogit(case ~ HI_lag_0 + strata(bene_id), data_onerace[Koppen_s =="Temperate"])
+  model_trop <- clogit(case ~ HI_lag_0 + strata(bene_id), data_onerace[Koppen_s =="Tropical"])
+  model_arid <- clogit(case ~ HI_lag_0 + strata(bene_id), data_onerace[Koppen_s =="Arid"])
+  model_cont <- clogit(case ~ HI_lag_0 + strata(bene_id), data_onerace[Koppen_s =="Continental"])
+  
+  summary(model_temp)
+  summary(model_trop)
+  summary(model_arid)
+  summary(model_cont)
+  
+  ##US Overall 
+  model_lin <- clogit(case ~ HI_lag_0 + strata(bene_id), data_onerace)
+  summary(model_lin)
+  
+  cp <- list()
+  for (c in unique(data_onerace[!is.na(Koppen_s), Koppen_s])) {
+    cat("\n", c)}
+  
+  
+  #Temperate model
+  c <- "Temperate"
+  
+  lag_dat_perc <- matrix(0.0, data_onerace[Koppen_s == c, .N], 15)
+  for (st in unique(data_onerace[Koppen_s == c, Koppen])) {
+    ec <- ecdf(data_onerace[Koppen == st]$HI_lag_0)
+    lag_dat_perc[which(data_onerace[Koppen_s == c, Koppen] == st), ] <-
+      sapply(data_onerace[Koppen == st, .SD, .SDcols = paste0("HI_lag_", 0:14)], ec)
+  }
+  
+  aic <- Inf
+  for (df_knot in c(.7, .75, .8, .85, .9)) {
+    for (df_lag in 3:5) {
+      cat("\n", df_knot, df_lag)
+      cb <- crossbasis(lag_dat_perc,
+                       lag = 14, 
+                       argvar = list(fun = "cr", knots = c(0, df_knot, 1)), 
+                       arglag = list(fun = "cr", 
+                                     knots = (exp(seq(0, log(15), length.out = df_lag)) - 1)))
+      model_temperate <- clogit(case ~ cb + strata(bene_id), 
+                                  data_onerace[Koppen_s == c])
+      
+      if (AIC(model_temperate) < aic) {
+        cat(" *")
+        aic <- AIC(model_temperate)
+        cp[[c]] <- crosspred(cb, model_temperate, at = 1:999/1000, cen = 0.5, cumul = TRUE)
+        cp[[c]]$df_knot <- df_knot
+        cp[[c]]$df_lag <- df_lag
+        best_model <- model_temperate
+        best_df_knot <- df_knot
+        best_df_lag <- df_lag
+      }
+    }
+  }
+  summary(model_temperate)
+  
+  cat("\nknots:", best_df_knot, "\n")
+  cat("chosen lag df:", best_df_lag, "\n")
+  summary(best_model)
+  
+  
+  
+  #Tropical model
+  c <- "Tropical"
+  
+  lag_dat_perc <- matrix(0.0, data_onerace[Koppen_s == c, .N], 15)
+  for (st in unique(data_onerace[Koppen_s == c, Koppen])) {
+    ec <- ecdf(data_onerace[Koppen == st]$HI_lag_0)
+    lag_dat_perc[which(data_onerace[Koppen_s == c, Koppen] == st), ] <-
+      sapply(data_onerace[Koppen == st, .SD, .SDcols = paste0("HI_lag_", 0:14)], ec)
+  }
+  
+  aic <- Inf
+  for (df_knot in c(.7, .75, .8, .85, .9)) {
+    for (df_lag in 3:5) {
+      cat("\n", df_knot, df_lag)
+      cb <- crossbasis(lag_dat_perc,
+                       lag = 14, 
+                       argvar = list(fun = "cr", knots = c(0, df_knot, 1)), 
+                       arglag = list(fun = "cr", 
+                                     knots = (exp(seq(0, log(15), length.out = df_lag)) - 1)))
+      model_tropical <- clogit(case ~ cb + strata(bene_id), 
+                                 data_onerace[Koppen_s == c])
+      
+      if (AIC(model_tropical) < aic) {
+        cat(" *")
+        aic <- AIC(model_tropical)
+        cp[[c]] <- crosspred(cb, model_tropical, at = 1:999/1000, cen = 0.5, cumul = TRUE)
+        cp[[c]]$df_knot <- df_knot
+        cp[[c]]$df_lag <- df_lag
+        best_model <- model_tropical
+        best_df_knot <- df_knot
+        best_df_lag <- df_lag
+      }
+    }
+  }
+  summary(model_tropical)
+  
+  cat("\nknots:", best_df_knot, "\n")
+  cat("chosen lag df:", best_df_lag, "\n")
+  summary(best_model)
+  
+  
+  #Arid model
+  c <- "Arid"
+  
+  lag_dat_perc <- matrix(0.0, data_onerace[Koppen_s == c, .N], 15)
+  for (st in unique(data_onerace[Koppen_s == c, Koppen])) {
+    ec <- ecdf(data_onerace[Koppen == st]$HI_lag_0)
+    lag_dat_perc[which(data_onerace[Koppen_s == c, Koppen] == st), ] <-
+      sapply(data_onerace[Koppen == st, .SD, .SDcols = paste0("HI_lag_", 0:14)], ec)
+  }
+  
+  aic <- Inf
+  for (df_knot in c(.7, .75, .8, .85, .9)) {
+    for (df_lag in 3:5) {
+      cat("\n", df_knot, df_lag)
+      cb <- crossbasis(lag_dat_perc,
+                       lag = 14, 
+                       argvar = list(fun = "cr", knots = c(0, df_knot, 1)), 
+                       arglag = list(fun = "cr", 
+                                     knots = (exp(seq(0, log(15), length.out = df_lag)) - 1)))
+      model_Arid <- clogit(case ~ cb + strata(bene_id), 
+                             data_onerace[Koppen_s == c])
+      
+      if (AIC(model_Arid) < aic) {
+        cat(" *")
+        aic <- AIC(model_Arid)
+        cp[[c]] <- crosspred(cb, model_Arid, at = 1:999/1000, cen = 0.5, cumul = TRUE)
+        cp[[c]]$df_knot <- df_knot
+        cp[[c]]$df_lag <- df_lag
+        best_model <- model_Arid
+        best_df_knot <- df_knot
+        best_df_lag <- df_lag
+      }
+    }
+  }
+  summary(model_Arid)
+  
+  cat("\nknots:", best_df_knot, "\n")
+  cat("chosen lag df:", best_df_lag, "\n")
+  summary(best_model)
+  
+  
+  
+  #Continental model
+  c <- "Continental"
+  
+  lag_dat_perc <- matrix(0.0, data_onerace[Koppen_s == c, .N], 15)
+  for (st in unique(data_onerace[Koppen_s == c, Koppen])) {
+    ec <- ecdf(data_onerace[Koppen == st]$HI_lag_0)
+    lag_dat_perc[which(data_onerace[Koppen_s == c, Koppen] == st), ] <-
+      sapply(data_onerace[Koppen == st, .SD, .SDcols = paste0("HI_lag_", 0:14)], ec)
+  }
+  
+  aic <- Inf
+  for (df_knot in c(.7, .75, .8, .85, .9)) {
+    for (df_lag in 3:5) {
+      cat("\n", df_knot, df_lag)
+      cb <- crossbasis(lag_dat_perc,
+                       lag = 14, 
+                       argvar = list(fun = "cr", knots = c(0, df_knot, 1)), 
+                       arglag = list(fun = "cr", 
+                                     knots = (exp(seq(0, log(15), length.out = df_lag)) - 1)))
+      model_Continental  <- clogit(case ~ cb + strata(bene_id), 
+                                     data_onerace[Koppen_s == c])
+      
+      if (AIC(model_Continental) < aic) {
+        cat(" *")
+        aic <- AIC(model_Continental)
+        cp[[c]] <- crosspred(cb, model_Continental, at = 1:999/1000, cen = 0.5, cumul = TRUE)
+        cp[[c]]$df_knot <- df_knot
+        cp[[c]]$df_lag <- df_lag
+        best_model <- model_Continental
+        best_df_knot <- df_knot
+        best_df_lag <- df_lag
+      }
+    }
+  }
+  summary(model_Continental)
+  
+  
+  cat("\nknots:", best_df_knot, "\n")
+  cat("chosen lag df:", best_df_lag, "\n")
+  summary(best_model)
+  
+  
+  # Nationwide: all climate types
+  lag_dat_perc2 <- matrix(0.0, data_onerace[, .N], 15)
+  for (st in unique(data_onerace[!is.na(Koppen), Koppen])) {
+    ec <- ecdf(data_onerace[Koppen == st]$HI_lag_0)
+    lag_dat_perc2[which(data_onerace[, Koppen] == st), ] <-
+      sapply(data_onerace[Koppen == st, .SD, .SDcols = paste0("HI_lag_", 0:14)], ec)
+  }
+  c <- c("Temperate", "Tropical", "Arid", "Continental")
+  
+  aic <- Inf
+  cp_all <- list()
+  for (df_knot in c(.7, .75, .8, .85, .9)) {
+    for (df_lag in 3:5) {
+      cat("\n", df_knot, df_lag)
+      cb <- crossbasis(lag_dat_perc2,
+                       lag = 14, 
+                       argvar = list(fun = "cr", knots = c(0, df_knot, 1)),
+                       arglag = list(fun = "cr", 
+                                     knots = (exp(seq(0, log(15), length.out = df_lag)) - 1)))
+      model_nat <- clogit(case ~ cb + strata(bene_id), data_onerace)
+      
+      if (AIC(model_nat) < aic) {
+        cat(" *")
+        aic <- AIC(model_nat)
+        cp_all <- crosspred(cb, model_nat, at = 1:999/1000, cen = 0.5, cumul = TRUE)
+        cp_all$df_lag <- df_lag
+        cp_all$df_knot <- df_knot
+        best_model <- model_nat
+        best_df_knot <- df_knot
+        best_df_lag <- df_lag
+      }
+    }
+  }
+  summary(model_nat)
+  
+  cat("\nknots:", best_df_knot, "\n")
+  cat("chosen lag df:", best_df_lag, "\n")
+  summary(best_model)
+  
+  save(cp, cp_all, file = paste0("data/aic_models_", race_labels[i], ".rda"))
+  
+  gc()
+}
+
+
---
+++ analysis/Race_stratified_plot.R
@@ -0,0 +1,151 @@
+
+# libraries
+library(data.table)
+library(fst)
+library(ggplot2)
+library(tidyverse)
+library(RColorBrewer)
+
+# paths
+dir_data <- "/n/dominici_nsaph_l3/Lab/projects/cdinehart_PDhosp_temperature/data/"
+dir_figs <- "/n/dominici_nsaph_l3/Lab/projects/cdinehart_PDhosp_temperature/figures/"
+
+# load models from race-specific sub-analyses
+files <- c(paste0("data/aic_models_", 
+                  c("white", "black", "other", "asian", "hispanic", "native"),
+                  ".rda"))
+
+# Initialize list
+cp_list <- list()
+
+# Load each file into a temporary environment
+for (f in files) {
+  load(f)
+  race_name <- race_name <- sub("^.*aic_models_(.*)\\.rda$", "\\1", f)
+  
+  cp_list[[race_name]] <- c(cp, Nationwide = list(cp_all))
+  rm(cp, cp_all)
+}
+
+# percentiles
+perc <- 1:999/1000
+
+
+#---------- get data by climate type for each race
+
+dat <- rbindlist(lapply(names(cp_list), function(r) {
+  rbindlist(lapply(names(cp_list[[r]]), function(c) {
+  rbindlist(lapply(c(500, 900, 950, 990), function(p) {
+    data.frame(ctype = c,
+               race = r,
+               perc = perc[p],
+               val = cp_list[[r]][[c]]$predvar[p],
+               lag = 0:14,
+               fit = cp_list[[r]][[c]]$cumfit[p,],
+               se = cp_list[[r]][[c]]$cumse[p,])
+    }))
+  }))
+}))
+
+
+
+########################################################################
+####################### cumulative OR ############################
+########################################################################
+
+
+# restrict to rows of interest
+dat_cum <- dat[lag == 2 & val == 0.990]
+
+# new columns for OR and 95% CI
+dat_cum <- dat_cum %>%
+  mutate(or = exp(fit),
+         or_low = exp(fit - 1.96 * se),
+         or_high = exp(fit + 1.96 * se))
+
+# make climate type factor to set order
+dat_cum$ctype <- factor(dat_cum$ctype,
+                         levels = c("Nationwide", "Temperate", "Continental", "Arid", "Tropical"))
+
+dat_cum$race <- factor(dat_cum$race,
+                             levels = c("white", "black", "asian", "hispanic", "native", "other"),
+                             labels = c("White", "Black", "Asian/PI", "Hispanic", "AI/AN", "Other"))
+
+# plot
+pdf(paste0(dir_figs, "race_cumulative.pdf"), height = 3.5, width = 8)
+dat_cum |>
+  ggplot(aes(x = race, y = or, 
+             color = race, group = race,
+             ymin = or_low, ymax = or_high)) +
+  geom_hline(yintercept = 1, linetype = 2, linewidth = 0.5, color = "gray50") +
+  geom_errorbar(linewidth = 0.7, width = 0) +
+  geom_point(size = 2) +
+  scale_color_brewer(palette = "Dark2") +
+  coord_cartesian(ylim = c(0, 2.5)) +
+  theme_light(base_size = 10) +
+  theme(
+    #axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank(),
+    #axis.line = element_line(),
+    #axis.ticks = element_line(),
+    #panel.spacing = unit(1, "lines"),
+    strip.background = element_blank(),
+    strip.text = element_text(color = "black")
+  ) +
+  facet_grid(~ ctype) +
+  labs(x = "", 
+       y = "Odds ratio", 
+       color = "", 
+       fill = "")
+dev.off()
+
+
+########################################################################
+####################### lag plots ############################
+########################################################################
+
+# select percentile of interest
+pctile <- 990
+
+# nationwide
+dat_lag <- rbindlist(lapply(names(cp_list), function(r) {
+  data.frame(race = r,
+             lag = 0:14,
+             fit = cp_list[[r]][["Nationwide"]]$matfit[pctile, ],
+             se = cp_list[[r]][["Nationwide"]]$matse[pctile, ])
+}))
+
+dat_lag$race <- factor(dat_lag$race,
+                       levels = c("white", "black", "asian", "hispanic", "native", "other"),
+                       labels = c("White", "Black", "Asian/PI", "Hispanic", "AI/AN", "Other"))
+
+# new columns for OR and 95% CI
+dat_lag <- dat_lag %>%
+  mutate(or = exp(fit),
+         or_low = exp(fit - 1.96 * se),
+         or_high = exp(fit + 1.96 * se))
+
+pdf(paste0(dir_figs, "race_lag.pdf"), height = 3.5, width = 8)
+dat_lag %>%
+  ggplot(aes(x = lag, y = or)) +
+  geom_hline(yintercept = 1, col = "gray50", lty = 2) +
+  geom_line(col = "orangered") +
+  geom_ribbon(aes(ymin = or_low, ymax = or_high), fill = "orangered", alpha = 0.3) +
+  labs(#title = "Figure 2(a). Lag-response relationship for one day of exposure to the 99th versus the 50th percentile of heat index \ndistributions.",
+       x = "Lag days (days after exposure)",
+       y = "Odds ratio") +
+  facet_grid(~race) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0),
+                     breaks = scales::pretty_breaks()) +
+  #scale_fill_manual(values = ctype_cols) +
+  #scale_color_manual(values = ctype_cols) +
+  #coord_cartesian(ylim = c(0.95, 1.04)) +
+  theme_light() +
+  theme(legend.position = "none",
+        strip.background = element_blank(),
+        strip.text = element_text(color = "black"),
+        plot.title = element_text(size = 10),
+        plot.margin = margin(10, 10, 25, 10)) # extra space below
+dev.off()
